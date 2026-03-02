03c02489180890b2cd03d86a816af6889ff7094a
Alejandra <alejandrajd16@gmail.com>
2026-03-02 14:49:14 -0500

    Solucion de error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("GRUPO 2")
 
 for i in range(3):
     pin = input("Ingrese el pin ")
-    if pin== "123":
+    if pin== "1234":
         print("¡Contraseña correcta! Acceso concedido.")
     else:
         print("Contraseña incorrecta. Inténtalo de nuevo.")
