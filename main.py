6819b740858b4807ac25d513323867c1ca677c08
Alejandra <alejandrajd16@gmail.com>
2026-03-02 14:03:49 -0500

    Control de intentos

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("GRUPO 2")
+for i in range(3):
+    pin = input("Ingrese el pin ")
+    if pin== "123":
+        print("¡Contraseña correcta! Acceso concedido.")
+    else:
+        print("Contraseña incorrecta. Inténtalo de nuevo.")
+else:
+    print("Has agotado tus intentos. Acceso denegado.")
