c220c1b6d149abaf46b45d7dbea4812c00c8371a
Nelson <nelbalvin2005@gmail.com>
2026-03-02 14:22:45 -0500

    Validacion Retiros

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("GRUPO 2")
+
+
+saldo = int(1000)
+retiro = int    
+if retiro <= 0:
+    print("Pro favor infresar un Valor Valido")
+    
+elif retiro > saldo:
+    print("Fondos insuficientes")
+    
+else: retiro <= saldo
