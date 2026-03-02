215b034d99e00f708aadb4485a10c582573b2418
jayala341 <jhoansebastianayala@gmail.com>
2026-03-02 14:30:58 -0500

    funcion de retiro

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("GRUPO 2")
+
+from retiro import retirar_dinero
+
+valor=int(input("Ingrese el valor a retirar: "))
+saldo=1000
+print(retirar_dinero(valor, saldo))
---
+++ retiro.py
@@ -0,0 +1,3 @@
+def retirar_dinero(valor, saldo):
+    saldo -= valor
+    print(f"Has retirado {valor}. Tu nuevo saldo es {saldo}.")
