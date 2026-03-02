2e0b410949e50201373d753cd20168cf1f0551f0
oseph080508 <joseph.080508@gmail.com>
2026-03-02 14:22:26 -0500

    nuevo funcion de depostar

--- deposito.py
+++ deposito.py
@@ -3,3 +3,4 @@ def despositar( valor, saldo ):
         saldo+= valor
         print ("Nuevo saldo es:", saldo) 
         print("Gracias Por Usar el cajero")
+        print("GRUPO 2")
--- main.py
+++
@@ -1,1 +0,0 @@
-print("GRUPO 2")
