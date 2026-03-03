4d07ba949cd089b63729f0b2e4e7a62e8d80cb30
Juan Bolivar <jebolivarp23@gmail.com>
2026-03-03 11:50:56 -0500

    FUNCION RETIRAR HECHA

--- features.py
+++ features.py
@@ -1,1 +1,18 @@
-print("ESTE ES EL ARCHIVO PARA TODOS LOS FEATURES")
+
+
+def retiro(saldo):
+        Validador = True
+        Retiro = float(input("Ingrese monto a retirar: "))
+        while Validador:
+            if Retiro < 0:
+                return f"Ingrese una cantidad valida"
+                Retiro = float(input("Ingrese monto a retirar: "))
+            if Retiro > 0:
+                    Validador = False
+                    if Retiro > saldo:
+                        return f"Fondos insuficientes"
+                    elif Retiro <= saldo:
+                        return f"Retiro efectuado, su nuevo saldo es de: {(saldo - Retiro)}"
+                        
+
+
