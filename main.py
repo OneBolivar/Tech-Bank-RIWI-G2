4f2890da7069a370e89f2e7b6639f1ee9a83f467
Daniel <danielmendoza1515@gmail.com>
2026-03-03 11:26:50 -0500

    actualizacion del archivo main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from retiro_diario import control_de_retiro
 saldo = float(1000)
 print("...: Bievenidos al cajero automatico TechBank Riwi Digital:...")
 
-"""codigo = input("Ingrese el pin: ")
+codigo = input("Ingrese el pin: ")
 
 if codigo == "1234":
     print("Autenticado")
@@ -19,24 +19,6 @@ for i in range(3):
 else:
     print("Has agotado tus intentos. Acceso denegado. ")
 
-    """
-cuenta={
-     'Saldo':1000,
-     'Limite_de_retiro':500,
-     'retiro_diario':0
-}
-control_de_retiro(550,cuenta)
-print(cuenta)
-print("=====================")
-control_de_retiro(200,cuenta)
-print(cuenta)
-print("=====================")
-control_de_retiro(10,cuenta)
-print(cuenta)
-print("=====================")
-control_de_retiro(600,cuenta)
-print(cuenta)
-print("=====================")
-control_de_retiro(80,cuenta)
-print(cuenta)
-print("=====================")
+
+
+
