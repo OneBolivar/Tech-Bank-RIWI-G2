cc6d4cb0e30ad65c42d1c3c904b192e2e39e06c4
Luis Medrano <Lmedrano2314@gmail.com>
2026-03-03 12:46:55 -0500

    Segunda configuracion menu

--- menu.py
+++ menu.py
@@ -1,10 +1,14 @@
+from features import retiro, deposito
 def menu(saldo):
     Ejecucion = int(input("Elija una opcion: 1. Consultar saldo|| 2. Retirar dinero || 3. Depositar dinero: "))
     if Ejecucion == 1:
         return f"Su saldo actual es de: {saldo}"
     elif Ejecucion == 2:
-        print("bloque 2")             
+        return retiro(saldo)           
     elif Ejecucion == 3: 
-        print("bloque 3")
+        return deposito(saldo)
     else:
          print("Opcion invalida")
+prueba = menu(1000)
+print(prueba)
+         
