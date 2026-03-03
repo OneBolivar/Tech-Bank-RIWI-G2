259e77edf004fe7cbbc9d71b904acbf3d9771273
ricardo <ricardollanos711@gmail.com>
2026-03-03 15:04:25 -0500

    mensaje de terminal donde indica que se excede el monto

--- validacion_reglas_negocio.py
+++ validacion_reglas_negocio.py
@@ -1,3 +1,6 @@
 # Creando validacion de reglas de negocio
 # REGLA: El monto ingresado no debe ser mayor al saldo 
-
+def validacion_limite_retiro (monto, saldo):
+    while True:
+        if monto > saldo:
+            print("Monto excede el saldo en cuenta")
