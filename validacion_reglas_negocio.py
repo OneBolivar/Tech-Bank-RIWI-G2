a589e409fcf806b0f7afd400ea835a4f194ba6fa
ricardo <ricardollanos711@gmail.com>
2026-03-03 15:09:08 -0500

    Terminada funcion de validacion de limite del monto a retirar

--- validacion_reglas_negocio.py
+++ validacion_reglas_negocio.py
@@ -4,3 +4,9 @@ def validacion_limite_retiro (monto, saldo):
     while True:
         if monto > saldo:
             print("Monto excede el saldo en cuenta")
+            monto = int(input("Por favor ingresa el monto a retirar nuevamente: "))
+        else: 
+            print("Monto a retirar valido")
+            break
+
+
