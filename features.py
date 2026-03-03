c767b7c6baef5c15eb98bbf127cfaa8871bfb38d
Juan Bolivar <jebolivarp23@gmail.com>
2026-03-03 12:05:01 -0500

    Funciones 2 y 3

--- features.py
+++ features.py
@@ -22,8 +22,7 @@ def deposito(saldo):
         if Deposito < 0:
             print("Ingrese una cantidad valida")
             Deposito = float(input("Ingrese monto a depositar: "))
-            if Deposito >= 0:
-                Validador = False
-                return f"Su saldo total es de:  {(Deposito + saldo)}"
-                                
+        if Deposito >= 0:
+            Validador = False
+            return f"Su saldo total es de: {(Deposito + saldo)}"
 
