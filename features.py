e6b6282501ec33f46bbb7002fe57122c2ded868c
Juan Bolivar <jebolivarp23@gmail.com>
2026-03-03 12:08:25 -0500

    5 (FUNCIONES 2 y 3 (COMPLETADAS))

--- features.py
+++ features.py
@@ -5,14 +5,14 @@ def retiro(saldo):
         Retiro = float(input("Ingrese monto a retirar: "))
         while Validador:
             if Retiro < 0:
-                return f"Ingrese una cantidad valida"
+                print("Ingrese una cantidad valida")
                 Retiro = float(input("Ingrese monto a retirar: "))
             if Retiro > 0:
-                    Validador = False
                     if Retiro > saldo:
                         return f"Fondos insuficientes"
                     elif Retiro <= saldo:
                         return f"Retiro efectuado, su nuevo saldo es de: {(saldo - Retiro)}"
+                    Validador = False
                         
 
 def deposito(saldo):
@@ -26,3 +26,5 @@ def deposito(saldo):
             Validador = False
             return f"Su saldo total es de: {(Deposito + saldo)}"
 
+prueba = retiro(500)
+print(prueba)
