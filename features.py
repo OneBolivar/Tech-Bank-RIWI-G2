205ff54e391ae25810824886c02f8e4318527030
Juan Bolivar <jebolivarp23@gmail.com>
2026-03-03 12:00:55 -0500

    FUNCIONES 2 y 3 (ARREGLAR #3)

--- features.py
+++ features.py
@@ -15,4 +15,15 @@ def retiro(saldo):
                         return f"Retiro efectuado, su nuevo saldo es de: {(saldo - Retiro)}"
                         
 
+def deposito(saldo):
+    Validador = True
+    while Validador:
+        Deposito = float(input("Ingrese monto a depositar: "))
+        if Deposito < 0:
+            print("Ingrese una cantidad valida")
+            Deposito = float(input("Ingrese monto a depositar: "))
+            if Deposito >= 0:
+                Validador = False
+                return f"Su saldo total es de:  {(Deposito + saldo)}"
+                                
 
