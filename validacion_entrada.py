672770e2c1d2f9e0e9d2e58337160fa8e78a6c6f
ricardo <ricardollanos711@gmail.com>
2026-03-03 14:27:56 -0500

    Terminada funcion de validacion para numeros negativos en opcion retiro

--- validacion_entrada.py
+++ validacion_entrada.py
@@ -1,4 +1,14 @@
 # VALIDACION NUMERO NEGATIVO
 
+# Se crea funcion para retiro 
+def validacion_retiro(monto):
+    while True:
+        if monto < 0:
+            print("Monto negativo")
+            monto = int(input("Por favor ingrese el monto correctamente: "))
+        else:
+            print("ingreso correctamente el monto a retirar:", monto)
+            break
+
 
     
