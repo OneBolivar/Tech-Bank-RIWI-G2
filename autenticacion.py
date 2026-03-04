04fc3fe2847a6fd810bd1b48c9aef70b1e565957
Luis Medrano <Lmedrano2314@gmail.com>
2026-03-04 12:43:22 -0500

    Añadida validacion de tipo numerico #1

--- autenticacion.py
+++ autenticacion.py
@@ -6,13 +6,16 @@ def autenticacion(cuenta):
         if pin==cuenta['Pin']:
             print("¡Contraseña correcta! Acceso concedido.")
             print("*" * 40,"\n")
-            intentos= int(input("¿Cuantas operaciones quieres realizar?: "))
-            for i in range (intentos):
-                  menu(cuenta)
-                  continuacion = int(input("\nQuieres continuar con las operaciones, 1 para si o 2 para no: \n"))
+            try:
+                intentos= int(input("¿Cuantas operaciones quieres realizar?: "))
+                for i in range (intentos):
+                    menu(cuenta)
+                    continuacion = int(input("\nQuieres continuar con las operaciones, 1 para si o 2 para no: \n"))
 
-                  if (continuacion==2): break
-            break
+                    if (continuacion==2): break
+                break
+            except:
+                print("Entrada no válida. Por favor, ingresa un número entero.")
         elif pin !=cuenta['Pin']:
             print("Contraseña incorrecta. Inténtalo de nuevo.")
      else:
