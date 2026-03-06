d24fdcef60e036bf68efe1d862cdbda53da1d808
Joseph <joseph.080508@gmail.com>
2026-03-05 22:52:40 -0500

    funcion de deposito

--- deposito.py
+++
@@ -1,6 +0,0 @@
-from historial import historial_operaciones
-
-def despositar( valor, cuenta ):
-        cuenta['Saldo']+= valor
-        print (" Su Deposito fue exitoso, su nuevo saldo es:", cuenta['Saldo']) 
-        historial_operaciones("Depositar", valor, cuenta['Saldo'])
